ab314d4187999529f03553fa5addeab113bb3f14
Vladislav Rogovski <bortmex19780@gmail.com>
2024-08-13 20:58:18 +0300

    example

--- 7kyu/is-it-a-letter.js
+++ 7kyu/is-it-a-letter.js
@@ -7,3 +7,11 @@
 function isItLetter(character) {
    return typeof character === 'string' && character.length === 1 && /[a-zA-Z]/.test(character);
 }
+
+const arr = [
+   9, 45, 53, 10, 100, 30, 85, 72, 69, 93, 98, 27, 73, 82, 91, 60, 5, 79, 88, 18, 71, 36, 44, 22, 89, 40, 59, 80, 81, 67, 25, 54, 13,
+];
+const arr2 = [];
+arr2.concat(arr, arr2);
+arr2.sort((a, b) => a - b);
+console.log(arr2);
